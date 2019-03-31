b83f71471074a7cff5cd1f8af573431826a4d590
AnhQuangCee <duonganhquang97@gmail.com>
2019-03-31 13:13:11 +0700

    Complete ControlByHand, Automation with feature read data from NodeMcu

---
+++ index.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var app = express();
+app.use(express.static("./public"));
+app.set("view engine","ejs");
+app.set("views","./views");
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+var ip = require('ip');
+const PORT = 3000;
+server.listen(PORT);
+console.log("Server nodejs chay tai dia chi: " + ip.address() + ":" + PORT);
+
+
+//Khi có mệt kết nối được tạo giữa Socket Client và Socket Server
+io.on('connection', function(socket) {	
+	var led = [true, false] //định nghĩa một mảng 1 chiều có 2 phần tử: true, false. Mảng này sẽ được gửi đi nhằm thay đổi sự sáng tắt của 2 con đèn LED đỏ và xanh. Dựa vào cài đặt ở Arduino mà đèn LEd sẽ bị bật hoặc tắt. Hãy thử tăng hoạt giảm số lượng biến của mảng led này xem. Và bạn sẽ hiểu điều kỳ diệu của JSON!
+	//Tạo một chu kỳ nhiệm vụ sẽ chạy lại sau mỗi 200ms
+	var interval1 = setInterval(function() {
+		for (var i = 0; i < led.length; i++) {
+			led[i] = !led[i]
+		}
+		
+		//Cài đặt chuỗi JSON, tên biến JSON này là json 
+		var json = {
+			"led": led, //có một phần tử là "led", phần tử này chứa giá trị của mảng led.
+			// "content": "hello"
+		}
+		// socket.emit('LED', json) //Gửi lệnh LED với các tham số của của chuỗi JSON
+		socket.on("client-send-data",function(dulieu){
+            // console.log(dulieu); //Du lieu nay la thong so cam bien
+            socket.broadcast.emit("Server-send-data", dulieu);
+        });
+		// console.log("send LED")//Ghi ra console.log là đã gửi lệnh LED
+	}, 2000)//200ms
+	
+	//Khi socket client bị mất kết nối thì chạy hàm sau.
+	socket.on('disconnect', function() {
+		console.log("Client Disconnect" + socket.id) ;	//in ra màn hình console cho vui
+		clearInterval(interval1);		//xóa chu kỳ nhiệm vụ đi, chứ không xóa là cái task kia cứ chạy mãi thôi đó!
+    });
+    
+    // Connect with Website
+    console.log("Client Connect: " + socket.id);
+    // socket.on("client-send-data",function(data){
+    //     console.log(data);
+    // });
+    // socket.emit("Server-send-data",{ hello: 'world' });
+});
+
+app.get("/",function(req,res){
+    res.render("controlByHand");
+})
---
+++ public/javascripts/js/Automation.js
@@ -0,0 +1,98 @@
+//Processbar
+var bar = new ProgressBar.Circle(template, {
+  color: '#FF7043',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: false
+  },
+  from: { color: '#FF7043', width: 4 },
+  to: { color: '#FF7043', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '50px';
+
+
+
+////////////////
+var bar1 = new ProgressBar.Circle(humidity, {
+  color: '#0277BD',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: false
+  },
+  from: { color: '#0277BD', width: 4 },
+  to: { color: '#0277BD', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar1.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar1.text.style.fontSize = '50px';
+
+
+//Socket
+var socket = io("http://localhost:3000");
+socket.on("Server-send-data",function(data){
+  // $("#noidung").append(data);
+  console.log(typeof data.humidity);
+  // document.getElementById("template").innerHTML = "Temperature: "+ data.temperature;
+  // document.getElementById("humidity").innerHTML = "Humidity: "+ data.humidity;
+  // socket.emit("client-send-data",{ my: 'data' });
+  bar.animate((data.temperature)/100);  // Number from 0.0 to 1.0
+  bar1.animate((data.humidity)/100);  // Number from 0.0 to 1.0  
+
+  //Light
+  
+  if(data.light == 1){
+    document.getElementById("light").innerHTML = "Led tat";
+  }
+  else if(data.light == 0){
+    document.getElementById("light").innerHTML = "Led sang";    
+  }
+
+  //Soil Moisture
+  document.getElementById("soil").innerHTML = data.SoilMoisture;
+});
+
+//DateTimePicker 
+$( document ).ready(function() {
+  $('#datetimepicker1').datetimepicker();
+});
+
+
+
+
+
